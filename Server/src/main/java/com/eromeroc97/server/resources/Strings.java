e9a55cdee68ac8f8457e89877db2933c062bd50d
eromeroc97 <e.romerociudad@outlook.com>
2023-05-11 12:03:08 +0200

    Inicio del proyecto
    Se inicia el proyecto desarrollando mediante arquitectura MVC utilizando Java (Maven) e implementando la parte Servidor de la aplicación.
    
    La idea es realizar la app servidor mediante dos interfaces: Texto y Gráfica (TextUI y GUI)
    
    Todo ello integrará una base de datos SQLite

---
+++ Server/src/main/java/com/eromeroc97/server/resources/Strings.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.eromeroc97.server.resources;
+
+/**
+ *
+ * @author InformaticoLuciana
+ */
+public interface Strings {
+    public static final String APP_NAME = "Kanban Project Manager";
+}
